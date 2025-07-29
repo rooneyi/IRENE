ee736b39501b73dbf4508a494044a93081d935b5
rooney kalumba <22ki129@esisalama.org>
2025-07-29 16:00:36 +0200

    Refresh the table feetypes

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -10,12 +10,20 @@ class StudentFactory extends Factory
 
     public function definition(): array
     {
+        $sections = [
+            1 => 'Maternelle',
+            2 => 'Primaire',
+            3 => 'Secondaire général',
+            4 => 'Technique',
+        ];
+        $section_id = $this->faker->randomElement(array_keys($sections));
         return [
             'nom' => $this->faker->lastName(),
             'prenom' => $this->faker->firstName(),
             'post_nom' => $this->faker->lastName(),
             'matricule' => strtoupper($this->faker->unique()->bothify('MAT###??')),
             'classe' => $this->faker->randomElement(['6ème', '5ème', '4ème', '3ème', '2nde', '1ère', 'Terminale']),
+            'section_id' => $section_id,
             'date_naissance' => $this->faker->date('Y-m-d', '-10 years'),
             'sexe' => $this->faker->randomElement(['M', 'F']),
             'adresse' => $this->faker->address(),
